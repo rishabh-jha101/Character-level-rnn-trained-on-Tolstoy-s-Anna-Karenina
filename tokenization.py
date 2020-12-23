dd2e05636a7689669e3c6378b4f3575f13a5869f
Rishabh Jha <rishabhjha2203@gmail.com>
2020-12-23 19:37:31 +0530

    encoded the data: Tolstoy's Anna Karenina

--- tokenization.py
+++ tokenization.py
@@ -4,7 +4,17 @@ import numpy as np
 # open text file and read in data as `text`
 with open('data/anna.txt', 'r') as f:
     text = f.read()
+# encode the text and map each character to an integer and vice versa
 
+# we create two dictionaries:
+# 1. int2char, which maps integers to characters
+# 2. char2int, which maps characters to unique integers
+chars = tuple(set(text))
+int2char = dict(enumerate(chars))
+char2int = {ch: ii for ii, ch in int2char.items()}
+
+# encode the text
+encoded = np.array([char2int[ch] for ch in text])
 
 def one_hot_encode(arr, n_labels):
 
